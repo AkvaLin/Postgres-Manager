bc6f0c7744682ac0bfb7390a25b5291b6681b246
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2023-05-15 14:27:32 +0300

    implemented login function add: login results enum, userdefaults extensions

--- Postgres iOS App/Postgres iOS App/LoginViewModel.swift
+++ Postgres iOS App/Postgres iOS App/LoginViewModel.swift
@@ -10,6 +10,14 @@ import PostgresKit
 
 class LoginViewModel: ObservableObject {
     
+    @Published var isSaveEnabled: Bool = false
+    
+    // Database settings
+    private let host = "db.tjytbokwkceokguvmtrh.supabase.co"
+    private let database = "postgres"
+    private let login = "postgres"
+    private let password = "xyhwu2-nesded-poWqad"
+    
     private var eventLoopGroup: MultiThreadedEventLoopGroup? = nil
     private let logger = Logger(label: "postgres-logger")
     private var connection: PostgresConnection? = nil
@@ -18,22 +26,83 @@ class LoginViewModel: ObservableObject {
         setEventLoopGroup()
     }
     
-    public func connect(login: String, password: String) async {
-        guard let sslContext = try? NIOSSLContext(configuration: .clientDefault) else { return }
+    public func connect(login: String, password: String, completion: @escaping (LoginResults) -> Void) async {
+        do {
+            try await connection?.close()
+        } catch let error {
+            print("cannot close connection: \(error)")
+            completion(.unknownError)
+        }
+        
         guard let eventLoopGroup = eventLoopGroup else { return }
         
         let configuration = PostgresConnection.Configuration(
-            host: "ep-shiny-sunset-642465.eu-central-1.aws.neon.tech",
-            username: login,
-            password: password,
-            database: "neondb",
-            tls: .require(sslContext)
+            host: host,
+            username: self.login,
+            password: self.password,
+            database: database,
+            tls: .disable
         )
         
         do {
             connection = try await PostgresConnection.connect(on: eventLoopGroup.next(), configuration: configuration, id: 1, logger: logger)
         } catch let error {
             print("error: \(error)")
+            completion(.connectionError)
+            return
+        }
+        
+        do {
+            let checkPassword = try await connection?.query("SELECT check_password(\(login), \(password))", logger: logger)
+            guard let checkPassword = checkPassword else { completion(.dataError); return }
+            do {
+                for try await result in checkPassword.decode((Bool).self) {
+                    if result {
+                        let rows = try await connection?.query("SELECT * FROM login_data WHERE login = \(login)", logger: logger)
+                        guard let rows = rows else { completion(.dataError); return }
+                        for try await row in rows {
+                            let roleCell = row.first { cell in
+                                cell.columnName == "role"
+                            }
+                            guard let roleCell = roleCell else {
+                                completion(.dataError)
+                                return
+                            }
+                            let role = try roleCell.decode((String).self)
+                            print("Login succed with role \(role)")
+                            do {
+                                let query = PostgresQuery(unicodeScalarLiteral: "SET ROLE \(role)")
+                                try await connection?.query(query, logger: logger)
+                                if isSaveEnabled {
+                                    UserDefaults.standard.login = login
+                                    UserDefaults.standard.password = password
+                                    UserDefaults.standard.role = role
+                                }
+                                completion(.success)
+                            } catch let error {
+                                print("Role didnt change: \(error)")
+                                clearData()
+                                completion(.connectionError)
+                            }
+                        }
+                    } else {
+                        print("Wrong password")
+                        clearData()
+                        completion(.passwordError)
+                        return
+                    }
+                }
+            } catch {
+                print("User doesnt exist")
+                clearData()
+                completion(.loginError)
+                return
+            }
+        } catch let error {
+            print("query error: \(error)")
+            clearData()
+            completion(.queryError)
+            return
         }
     }
     
@@ -41,5 +110,11 @@ class LoginViewModel: ObservableObject {
         DispatchQueue.global(qos: .background).async {
             self.eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
         }
+    }
+    
+    private func clearData() {
+        UserDefaults.standard.login = nil
+        UserDefaults.standard.password = nil
+        UserDefaults.standard.role = nil
     }
 }
